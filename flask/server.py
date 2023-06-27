d68c5de048b2a32f70678a36bb0eee048c300c2f
Roberto Rosa <roberto.rosa7@gmail.com>
2023-06-27 18:36:40 -0300

    update

--- flask/server.py
+++ flask/server.py
@@ -48,4 +48,4 @@ def audi_blob():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", ssl_context=('certificate.pem', 'privatekey.pem'))
